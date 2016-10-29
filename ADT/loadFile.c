f8e6d821c5e6736dd66b4be0f219e324e2e77643
AditPratama <aditya.psantoso@gmail.com>
2016-10-29 14:54:50 +0700

    a

---
+++ ADT/loadFile.c
@@ -0,0 +1,74 @@
+#include "loadfile.h"
+
+void loadEnemy(monster *M, int *jum,char text[100]) {
+	STARTKATA(text);
+	int i = 1;
+	int j, k;
+	k = 0;
+	(*jum) = 0;
+	while (!EndKata) {				
+		if (CKata.tanda == BINTANG) {	
+			for (k = 0; k < CKata.Length; k++) {
+				(*M).Mon[i].name[k] = CKata.TabKata[k];
+			}
+			(*M).Mon[i].name[k] = NULL;
+			(*jum) = (*jum) + 1;	
+			ADVKATA(text);
+		} else {
+			j = 1;
+			while (j <= 5) {
+				if (j == 1) {
+					(*M).Mon[i].level = CKata.angka;
+					j++;
+				} else if (j == 2) {
+					(*M).Mon[i].hp = CKata.angka;
+					j++;					
+				} else if (j == 3) {
+					(*M).Mon[i].str = CKata.angka;
+					j++;				
+				} else if (j == 4) {
+					(*M).Mon[i].def = CKata.angka;
+					j++;					
+				} else {
+					(*M).Mon[i].exp = CKata.angka;
+					j++;
+				}
+				ADVKATA(text);
+			}
+			i++;
+		}
+	}
+}
+
+void loadStatHero(hero *H, int *jum,char text[100]) {
+	STARTKATA(text);
+	int i = 1;
+	int j, k;
+	k = 0;
+	(*jum) = 0;
+	while (!EndKata) {				
+		j = 1;
+		while (j <= 5) {
+			if (j == 1) {
+				(*H).status[i].level = CKata.angka;
+				j++;
+			} else if (j == 2) {
+				(*H).status[i].hp = CKata.angka;
+				j++;					
+			} else if (j == 3) {
+				(*H).status[i].str = CKata.angka;
+				j++;				
+			} else if (j == 4) {
+				(*H).status[i].def = CKata.angka;
+				j++;					
+			} else {
+				(*H).status[i].exp = CKata.angka;
+				j++;
+			}
+			ADVKATA(text);
+		}
+		(*jum) = (*jum) + 1;
+		i++;
+	}
+}
+
---
+++ ADT/loadFile.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include "mesinkar.h"
+#include "mesinkata.h"
+
+#ifndef __LOADFILE_H_
+#define __LOADFILE_H_
+
+typedef struct {
+	int hp;
+	int str;
+	int def;
+	int exp;
+	int level;
+	char name[10];
+} statMonster;
+
+typedef struct {
+	statMonster Mon[10];
+} monster;
+
+typedef struct {
+	int hp;
+	int str;
+	int def;
+	int exp;
+	int level;
+} statHero;
+
+typedef struct {
+	char name[100];
+	statHero status[10];
+} hero;
+
+//char text[100] = nama file txt yang bakal lu load
+//int jum = jumlah monster / level hero yang ada
+void loadEnemy(monster *M, int *jum,char text[100]);
+
+void loadStatHero(hero *H, int *jum,char text[100]);
+
+#endif
