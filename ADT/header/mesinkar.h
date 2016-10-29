568f98e21ebe0d43d4c7278d1ee06265d44b9f2b
atianadya <theaolivia.mail@gmail.com>
2016-10-29 14:11:08 +0700

    ngerapiin doang

--- ADT/header/mesinkar.h
+++ ADT/header/mesinkar.h
@@ -1,34 +1,29 @@
-/* NIM/Nama		:  */
-/* Nama file	:  */
-/* Topik		:  */
-/* Tanggal		:  */
-/* Deskripsi	:  */
+/* File: mesinkar.h */
+/* Definisi Mesin Karakter */
 
-#ifndef mesinkar_h
-#define mesinkar_h
+#ifndef __MESIN_KAR_H_
+#define __MESIN_KAR_H_
 
-#include <stdio.h>
 #include "boolean.h"
 
 #define MARK '.'
-
 /* State Mesin */
 extern char CC;
 extern boolean EOP;
 
-void START();
-/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca. 
+void START(char text[100]);
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
    Karakter pertama yang ada pada pita posisinya adalah pada jendela.
    I.S. : sembarang
    F.S. : CC adalah karakter pertama pada pita
-   Jika CC != MARK maka EOP akan padam (false)
-   Jika CC = MARK maka EOP akan menyala (true) */
+          Jika CC != MARK maka EOP akan padam (false)
+          Jika CC = MARK maka EOP akan menyala (true) */
 
 void ADV();
 /* Pita dimajukan satu karakter. 
    I.S. : Karakter pada jendela = CC, CC != MARK
    F.S. : CC adalah karakter berikutnya dari CC yang lama, 
-   CC mungkin = MARK
-   Jika  CC = MARK maka EOP akan menyala (true) */
+          CC mungkin = MARK
+          Jika  CC = MARK maka EOP akan menyala (true) */
 
 #endif
