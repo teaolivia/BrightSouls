9f057f09af62656aebfda04af4a6c2ba6d7b6c28
KevinIswara <keviniswara18@gmail.com>
2016-11-14 09:31:36 +0700

    perbaikan map

--- ADT/map.c
+++ ADT/map.c
@@ -2,35 +2,38 @@
 #include "map.h"
 
 void BacaMap(AllMap *map, char text[100]){
-	int i,j;
+	int i,j,jmlhmap;
 	STARTKATA(text);
 	
-	JmlhMap(*map) = 0;
-	while(CC != '.'){
-		JmlhMap(*map)++;
-		i = 1;
-		while((CC != '/') && (CC != '.')){
-			j = 1;
-			while((CC != '-') && (CC != '.') && (CC != '/')){
-				Elmt(((*map).TabMap[JmlhMap(*map)]),i,j) = CKata.TabKata[0];
-				j++;
-				ADVKATA(text);
+	jmlhmap = 0;
+	if((CC != '.') && (CKata.TabKata[0] != '.')){
+		jmlhmap++;
+		while((CKata.TabKata[0]=='A') || (CKata.TabKata[0]=='B') || (CKata.TabKata[0]=='C') || (CKata.TabKata[0]=='D') || (CKata.TabKata[0]=='Z') || (CKata.TabKata[0]=='F') || (CKata.TabKata[0]=='G') || (CKata.TabKata[0]=='H')){
+			NomorMap(*map)[jmlhmap] = CKata.TabKata[0];
+			ADVKATA(text);
+			i = 1;
+			while((CC != '.') && (CKata.TabKata[0] != '/') && (CKata.TabKata[0] != '.')){
+				j = 1;
+				while((CC != '.') && (CKata.TabKata[0] != '-') && (CKata.TabKata[0] != '.') && (CKata.TabKata[0] != '/')){
+					ElmtMat(((*map).TabMap[jmlhmap]),i,j) = CKata.TabKata[0];
+					j++;
+					ADVKATA(text);
+				}
+				if((CC != '.') && (CKata.TabKata[0] != '/') && (CKata.TabKata[0] != MARK)){
+					ADVKATA(text);
+					i++;
+				}
+				else if (CC == MARK){
+					ElmtMat(((*map).TabMap[jmlhmap]),i,j) = CKata.TabKata[0];
+				}
 			}
-			if((CC != '/') && (CC != MARK)){
-				Elmt(((*map).TabMap[JmlhMap(*map)]),i,j) = CKata.TabKata[0];
-				ADV();
-				ADV();
+			NBrsEff((*map).TabMap[jmlhmap]) = i;
+			NKolEff((*map).TabMap[jmlhmap]) = j;
+			if(CC!=MARK){
 				ADVKATA(text);
-				i++;
-			}
-			else{
-				Elmt(((*map).TabMap[JmlhMap(*map)]),i,j) = CKata.TabKata[0];
+				jmlhmap++;
 			}
 		}
-		ADV();
-		ADV();
-		ADVKATA(text);
-		NBrsEff((*map).TabMap[JmlhMap(*map)]) = i;
-		NKolEff((*map).TabMap[JmlhMap(*map)]) = j;
 	}
+	JmlhMap(*map) = jmlhmap;
 }
--- ADT/map.h
+++ ADT/map.h
@@ -8,12 +8,14 @@
 
 typedef struct {
 	MATRIKS TabMap[NMax]; /* container penyimpan kata, indeks yang dipakai [0..NMax-1] */
+    char NomorMap[NMax];
     int JumlahMap;
 } AllMap;
 
 
 /* *** Selektor *** */
 #define JmlhMap(map) (map).JumlahMap
+#define NomorMap(map) (map).NomorMap
 
 void BacaMap(AllMap *map, char text[100]);
 /* Membaca file eksternal text dan memasukkannya ke dalam *map */
