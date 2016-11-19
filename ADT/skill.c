6570fdc040c379ca118b3d7e7af438eee4e3b7ac
atianadya <theaolivia.mail@gmail.com>
2016-11-19 17:37:59 +0700

    penjelasan order skill requirement ada di mskill.c

--- ADT/skill.c
+++ ADT/skill.c
@@ -1,9 +1,42 @@
 #include "skill.h"
+#include "bintree.h"
+#include "listrek.h"
+#include "loadFile.h"
+#include "boolean.h"
+#include <stdlib.h>
+#include <time.h>
+
+// mengecek apakah monster adalah boss
+boolean isBoss(monster m) {
+	if (m.boss)
+		return true;
+}
+
+void printSkill(currhero ch) {
+
+}
 
 // skill ningkatin attack
 // autoberserk - HP tinggal 5%, double attack
-void autoberserk (){
+void autoberserk (currhero ch){
+	int current;
+	monster m;
 
+	for (int i=0; i<10; i++) {
+		switch (ch.status[i].level) {
+			case 0: break;
+			case 1: 
+				if (current == ch.status[i].hp * 0.05) {
+					m.Mon[i].hp--;
+				}
+				break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+		}
+
+	}
 }
 // frenzy - ningkatin defense, strength, ningkatin HP, nambahin EXP
 int frenzyIncreaseDef(){
@@ -23,8 +56,22 @@ int frenzyIncreaseExp() {
 }
 // drop of youth - nambahin darah 5%
 // ide sementara nambahinnnya tiap round
-int dropOfYouth() {
+int dropOfYouth(currhero ch) {
+	for (int i=0; i<10; i++) {
+		switch (ch.status[i].level) {
+			case 0: break;
+			case 1: 
+				if (current == ch.status[i].hp * 0.05) {
+					m.Mon[i].hp--;
+				}
+				break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+		}
 
+	}
 }
 
 // reflect shield - kalau kita kena damage, musuh tersebut kena damage 5%
@@ -38,5 +85,21 @@ int reflectShieldEnemy() {
 
 // counter - ketika musuh nyerang dan kita block, block kita nullify attack musuh tersebut
 void counter() {
+	if (enemy) {
+		damage = nil;
+	}
+}
+
+
+/* ACTIVE SKILLS */
+// TP - pindah ke map berikutnya secara random
+// -- ACQUIRED AT HERO LEVEL 4 (Skill Tree Level 3 (tinggi ke-2))
+void teleport(currhero ch) {
+	
+}
+
+// ACQUIRED AT HERO LEVEL 2
+// add SP costs HP - Skill Tree level 2 (tinggi ke-1)
+int bloodRitual(currhero ch) {
 	
 }
--- ADT/skill.h
+++ ADT/skill.h
@@ -2,22 +2,57 @@
 #define SKILL_H
 
 #include "boolean.h"
-#include "pohon.h"
+#include "bintree.h"
+#include "listrek.h"
+#include "loadFile.h"
 
+
+// ========== SELEKTOR ===========
+hero h;
+currhero ch;
+monster m;
+statHero sh;
+statMonster sm;
+
+// ========== OPERATOR TAMBAHAN ==
+// mengecek apakah monster adalah boss
+boolean isBoss(monster m);
+void printSkill(currhero ch);
+
+// ========== METHOD UTAMA =======
 // skill ningkatin attack
 // autoberserk - HP tinggal 5%, double attack
-void autoberserk();
+// -- ACQUIRED AT HERO LEVEL 5 (Skill Tree Level 3 (tinggi ke-2))
+void autoberserk(currhero ch);
+
 // frenzy - ningkatin defense, strength, ningkatin HP, nambahin EXP
-int frenzyIncreaseDef();
-int frenzyIncreaseStr();
-int frenzyIncreaseHP();
-int frenzyIncreaseExp();
+// -- ACQUIRED AT HERO LEVEL 3 (Skill Tree Level 3 (tinggi ke-2))
+int frenzyIncreaseDef(currhero ch);
+int frenzyIncreaseStr(currhero ch);
+int frenzyIncreaseHP(currhero ch);
+int frenzyIncreaseExp(currhero ch);
+
 // drop of youth - nambahin darah 5%
 // ide sementara nambahinnnya tiap round
-int dropOfYouth();
+// -- ACQUIRED AT HERO LEVEL 5 (Skill Tree Level 3 (tinggi ke-2))
+int dropOfYouth(currhero ch);
+
 // reflect shield - kalau kita kena damage, musuh tersebut kena damage 5%
-int reflectShieldPlayer();
-int reflectShieldEnemy();
+// -- ACQUIRED AT HERO LEVEL 2 (Skill Tree Level 2 (tinggi ke-1))
+int reflectShieldPlayer(currhero ch);
+int reflectShieldEnemy(currhero ch);
+
 // counter - ketika musuh nyerang dan kita block, block kita nullify attack musuh tersebut, terus musuh darahnya kurang dikit 5%
-void counter();
+// -- ACQUIRED AT HERO LEVEL 1 (Skill Tree Level 1 (tinggi ke-0))
+void counter(currhero ch);
+
+/* ACTIVE SKILLS */
+// TP - pindah ke map berikutnya secara random
+// -- ACQUIRED AT HERO LEVEL 4 (Skill Tree Level 3 (tinggi ke-2))
+void teleport(currhero ch);
+
+// ACQUIRED AT HERO LEVEL 2
+// add SP costs HP - Skill Tree level 2 (tinggi ke-1)
+int bloodRitual(currhero ch);
+
 #endif
